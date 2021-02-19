8bbd4e345df1c39b3af7701cc8a396b0e39bc9a0
unknown <duykhanhtran1808@gmail.com>
2021-02-19 09:26:08 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var PORT = 3000 || process.env.PORT;
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/app-ads.txt', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app-ads.txt'));
+});
+
+app.listen(PORT, function() {
+    console.log('Server is running on PORT:',PORT);
+});
